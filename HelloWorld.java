348f9a14d7b20a84b5a91762d8351b90efb1e2a2
root <root@ip-172-31-24-187.us-west-2.compute.internal>
2016-07-01 17:56:41 -0400

    javaprogram

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
-public class HelloWorld { 
-   public static void main(String[] args) { 
-      System.out.println("Hello, World");
-   }
+package com.mkyong.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void printHello() {
+		System.out.println("Hello ! " + name);
+	}
 }
